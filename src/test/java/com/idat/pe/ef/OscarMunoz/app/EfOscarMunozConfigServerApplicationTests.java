9d0f2df9b453a43cea0cb1e3841c29fcf947c2a4
Oscar Fernando Muñoz Gayoso <oscarmg_4@outlook.com>
2022-12-13 23:31:04 -0500

    ecfinal

---
+++ src/test/java/com/idat/pe/ef/OscarMunoz/app/EfOscarMunozConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.pe.ef.OscarMunoz.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfOscarMunozConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
